3347bac0622597486baa885ce195ba59d3faee45
dudushy <eduardotalarico1337@gmail.com>
2022-09-21 22:15:20 -0300

    :+1: GitHub Copilot

--- src/main.py
+++ src/main.py
@@ -5,12 +5,41 @@ import os
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
 
+TITLE = """
+/----------------------------------------------------------------------------------\\
+|  Júlio César trocou cada letra do seu nome para as 3 seguintes do alfabeto.      |
+|                                                                                  |
+|  Transcreva as seguintes palavras:                                               |
+|  - Marrocos                                                                      |
+|  - Matemática                                                                    |
+|  - Qualidade                                                                     |
+|  - Copa do Mundo                                                                 |
+|  - Inconstitucionalíssimo                                                        |
+\\----------------------------------------------------------------------------------/
+"""
+
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+alfabeto3seguintes = "defghijklmnopqrstuvwxyzabc"
+
 #* -- Functions
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
+def julioCesar() -> None: #? Julio Cesar
+    clearConsole()
+    print(TITLE)
+    print("Resposta: ")
+    for palavra in input().split():
+        for letra in palavra:
+            if letra in alfabeto:
+                print(alfabeto3seguintes[alfabeto.index(letra)], end="")
+            else:
+                print(letra, end="")
+        print(" ", end="")
+    print()
+
 def main() -> None:
-    pass
+    julioCesar()
 
 #! Main
 if __name__ == "__main__":
